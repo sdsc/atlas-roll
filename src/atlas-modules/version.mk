f163ebedaca93d56b7448e702b1f7dc96e5fe1cc
Jim Hayes <jhayes@sdsc.edu>
2014-09-23 22:58:22 -0700

    Modulefile rpm build restructuring.
    Adopt/adapt Rocks approach to copying version from corresponding s/w package.
    Move all variant definitions into version.mk so that identical modulefile
    Makefile will work across rolls.

--- src/atlas-modules/version.mk
+++ src/atlas-modules/version.mk
@@ -1,6 +1,9 @@
-NAME       = atlas-modules_$(ROLLCOMPILER)
-VERSION    = 3.10.0
-RELEASE    = 1
-PKGROOT    = /opt/modulefiles/applications/.$(ROLLCOMPILER)/atlas
+NAME        = atlas-modules_$(ROLLCOMPILER)
+RELEASE     = 2
+PKGROOT     = /opt/modulefiles/applications/.$(ROLLCOMPILER)/atlas
 
-RPM.EXTRAS = AutoReq:No
+VERSION_SRC = $(REDHAT.ROOT)/src/atlas/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+RPM.EXTRAS  = AutoReq:No
